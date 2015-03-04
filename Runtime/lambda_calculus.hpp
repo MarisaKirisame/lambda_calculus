3d8ba0db935824cec6fa66e8c5a170f80af9040c
lolisa <lolisa@marisa.moe>
2015-03-04 10:28:32 -0500

    Fix bug.

--- Runtime/lambda_calculus.hpp
+++ Runtime/lambda_calculus.hpp
@@ -5,6 +5,7 @@
 #include <boost/variant.hpp>
 #include <boost/optional/optional.hpp>
 #include <utility>
+#include <map>
 namespace Runtime_lambda_calculus
 {
     struct expression;
@@ -13,7 +14,6 @@ namespace Runtime_lambda_calculus
     std::shared_ptr< const expression > app(
             const std::shared_ptr< const expression > & fst,
             const std::shared_ptr< const expression > & snd );
-    size_t i = 0;
     template< typename INIT, typename FUNCTION, typename FIRST >
     auto pack_fold_l( const INIT & init, const FUNCTION & function, const FIRST & first ) { return function( init, first ); }
 
@@ -73,62 +73,116 @@ namespace Runtime_lambda_calculus
                 std::shared_ptr< const expression > operator( )( const application & app ) const
                 { return Runtime_lambda_calculus::app( app.fst->substitute( i, exp ), app.snd->substitute( i, exp ) ); }
                 std::shared_ptr< const expression > operator( )( const var & i ) const
-                { return i.i == this->i ? exp.shared_from_this( ) : ::Runtime_lambda_calculus::var( i.i ); }
+                { return i.i == this->i ? exp.update_index( this->i, 0 ) : ::Runtime_lambda_calculus::var( ( i.i > this->i ) ? ( i.i - 1 ) : ( i.i ) ); }
             };
-            exp.shared_from_this( );
             substitute_visitor sv { i, exp };
-            return v.apply_visitor( sv );
+            auto res = v.apply_visitor( sv );
+            return res;
         }
-        bool operator == ( const expression & cmp ) const //it is impossible to decide semantically, so this decide if two expression are structrually equal
+        size_t num_of_abstraction_needed_to_form_closed_expression( size_t current_depth ) const
         {
-            struct equal_visitor_out : boost::static_visitor< bool >
+            struct visitor : boost::static_visitor< size_t >
+            {
+                size_t current_depth;
+                visitor( size_t current_depth ) : current_depth( current_depth ) { }
+                size_t operator( )( const abstraction & abs ) const
+                { return abs.abs->num_of_abstraction_needed_to_form_closed_expression( current_depth + 1 ); }
+                size_t operator( )( const application & app ) const
+                {
+                    return std::max(
+                                app.fst->num_of_abstraction_needed_to_form_closed_expression( current_depth ),
+                                app.snd->num_of_abstraction_needed_to_form_closed_expression( current_depth ) );
+                }
+                size_t operator( )( const var & i ) const
+                { return ( i.i > current_depth ) ? ( i.i - current_depth ) : 0; }
+            };
+            visitor vi { current_depth };
+            return v.apply_visitor( vi );
+        }
+        bool closed_expression( size_t current_depth ) const
+        {
+            struct closed_expression_visitor : boost::static_visitor< bool >
+            {
+                size_t current_depth;
+                closed_expression_visitor( size_t current_depth ) : current_depth( current_depth ) { }
+                bool operator( )( const abstraction & abs ) const
+                { return abs.abs->closed_expression( current_depth + 1 ); }
+                bool operator( )( const application & app ) const
+                { return app.fst->closed_expression( current_depth ) && app.snd->closed_expression( current_depth ); }
+                bool operator( )( const var & i ) const
+                { return i.i <= current_depth; }
+            };
+            closed_expression_visitor uiv { current_depth };
+            return v.apply_visitor( uiv );
+        }
+        std::shared_ptr< const expression > update_index( size_t i, size_t current_depth ) const
+        {
+            struct update_index_visitor : boost::static_visitor< std::shared_ptr< const expression > >
+            {
+                size_t i;
+                size_t current_depth;
+                update_index_visitor( size_t i, size_t current_depth ) : i( i ), current_depth( current_depth ) { }
+                std::shared_ptr< const expression > operator( )( const abstraction & abs ) const
+                { return Runtime_lambda_calculus::abs( abs.abs->update_index( i, current_depth + 1 ) ); }
+                std::shared_ptr< const expression > operator( )( const application & app ) const
+                { return Runtime_lambda_calculus::app( app.fst->update_index( i, current_depth ), app.snd->update_index( i, current_depth ) ); }
+                std::shared_ptr< const expression > operator( )( const var & i ) const
+                { return ::Runtime_lambda_calculus::var( ( i.i > current_depth ) ? ( i.i + this->i - 1 ) : i.i ); }
+            };
+            update_index_visitor uiv { i, current_depth };
+            return v.apply_visitor( uiv );
+        }
+        bool operator < ( const expression & cmp ) const //for sorting
+        {
+            struct lesser_visitor_out : boost::static_visitor< bool >
             {
                 const expression & cmp;
-                equal_visitor_out( const expression & cmp ) : cmp( cmp ) { }
+                lesser_visitor_out( const expression & cmp ) : cmp( cmp ) { }
                 bool operator( )( const abstraction & abs ) const
                 {
-                    struct abstraction_equal_visitor : boost::static_visitor< bool >
+                    struct abstraction_lesser_visitor : boost::static_visitor< bool >
                     {
                         const std::shared_ptr< const expression > & abs;
-                        abstraction_equal_visitor( const std::shared_ptr< const expression > & abs ) : abs( abs ) { }
-                        bool operator( )( const abstraction & abs ) const { return *this->abs == *abs.abs; }
+                        abstraction_lesser_visitor( const std::shared_ptr< const expression > & abs ) : abs( abs ) { }
+                        bool operator( )( const abstraction & abs ) const { return *this->abs < *abs.abs; }
                         bool operator( )( const application & ) const { return false; }
                         bool operator( )( const var & ) const { return false; }
                     };
-                    abstraction_equal_visitor aev( abs.abs );
-                    return cmp.v.apply_visitor( aev );
+                    abstraction_lesser_visitor alv { abs.abs };
+                    return cmp.v.apply_visitor( alv );
                 }
                 bool operator( )( const var & i ) const
                 {
-                    struct var_equal_visitor : boost::static_visitor< bool >
+                    struct var_lesser_visitor : boost::static_visitor< bool >
                     {
                         size_t i;
-                        var_equal_visitor( size_t i ) : i( i ) { }
-                        bool operator( )( const abstraction & ) const { return false; }
+                        var_lesser_visitor( size_t i ) : i( i ) { }
+                        bool operator( )( const abstraction & ) const { return true; }
                         bool operator( )( const application & ) const { return false; }
-                        bool operator( )( const var & i ) const { return this->i == i.i; }
+                        bool operator( )( const var & i ) const { return this->i < i.i; }
                     };
-                    var_equal_visitor iev { i.i };
+                    var_lesser_visitor iev { i.i };
                     return cmp.v.apply_visitor( iev );
                 }
                 bool operator( )( const application & app ) const
                 {
-                    struct application_equal_visitor : boost::static_visitor< bool >
+                    struct application_lesser_visitor : boost::static_visitor< bool >
                     {
                         const std::shared_ptr< const expression > & fst, snd;
-                        application_equal_visitor( const std::shared_ptr< const expression > & fst, const std::shared_ptr< const expression > & snd ) :
+                        application_lesser_visitor( const std::shared_ptr< const expression > & fst, const std::shared_ptr< const expression > & snd ) :
                             fst( fst ), snd( snd ) { }
-                        bool operator( )( const abstraction & ) const { return false; }
-                        bool operator( )( const application & app ) const { return *this->fst == *app.fst && *this->snd == *app.snd; }
-                        bool operator( )( const var & ) const { return false; }
+                        bool operator( )( const abstraction & ) const { return true; }
+                        bool operator( )( const application & app ) const { return std::tie( *this->fst, *this->snd ) < std::tie( *app.fst, *app.snd ); }
+                        bool operator( )( const var & ) const { return true; }
                     };
-                    application_equal_visitor aev( app.fst, app.snd );
-                    return cmp.v.apply_visitor( aev );
+                    application_lesser_visitor alv { app.fst, app.snd };
+                    return cmp.v.apply_visitor( alv );
                 }
             };
-            equal_visitor_out evo { cmp };
-            return v.apply_visitor( evo );
+            lesser_visitor_out lvo { cmp };
+            return v.apply_visitor( lvo );
         }
+        bool operator == ( const expression & cmp ) const { return ! (((*this) < cmp) || (cmp < (*this))); }
         bool operator != ( const expression & cmp ) const { return ! (*this == cmp); }
         boost::optional< abstraction > eval_abstraction( ) const
         {
@@ -142,7 +196,6 @@ namespace Runtime_lambda_calculus
                 }
                 boost::optional< abstraction >operator( )( const var & ) const { return boost::optional< abstraction >( ); }
             };
-            std::cout << ++i << std::endl;
             eval_abstraction_visitor eav;
             return v.apply_visitor( eav );
         }
@@ -163,6 +216,11 @@ namespace Runtime_lambda_calculus
             };
             eval_step_visitor esv;
             return v.apply_visitor( esv );
+        }
+        std::shared_ptr< const expression > eval( ) const
+        {
+            auto ret = eval_step( );
+            return *ret == *this ? ret : ret->eval( );
         }
     };
     std::shared_ptr< const expression > var( size_t i ) { return std::make_shared< expression >( expression::var( i ) ); }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Compiletime/example.hpp"
 #include "Runtime/lambda_calculus.hpp"
 #include "Runtime/Compiletime_Runtime.hpp"
+#include "cassert"
 #define Bridge( Name ) auto Name ## _ = Compiletime_Runtime_converter< Name >( )( )
 int main( )
 {
@@ -10,6 +11,7 @@ int main( )
     using Runtime_lambda_calculus::expression;
     using Runtime_lambda_calculus::Compiletime_Runtime_converter;
     using namespace Compiletime_lambda_calculus;
+    using namespace SKI_combinator_logic;
     Bridge( Null );
     Bridge( Nil );
     Bridge( Pair );
@@ -19,22 +21,23 @@ int main( )
     Bridge( Conditional );
     Bridge( True );
     Bridge( False );
-    auto program = abs( abs( abs( app(
+    Bridge( I );
+    Bridge( K );
+    Bridge( S );
+    auto program_inner = abs( abs( abs( app(
         Conditional_,
-        Null_,
+        app( Null_, var( 1 ) ),
         Nil_,
         app(
-            Conditional_,
+          Conditional_,
             app( var( 3 ), app( First_, var( 1 ) ) ),
             app( var( 2 ), app( Second_, var( 1 ) ) ),
             app( Pair_, app( First_, var( 1 ) ), app( var( 2 ), app( Second_, var( 1 ) ) ) ) ) ) ) ) );
-    auto list_ =
-        app( Pair_, app(Pair_, Nil_, Nil_), Nil_);
-    auto current = app(
-        program,
-        Null_,
-        Y_,
-        app(Pair_, Nil_, list_));
+    auto list_i = app( Pair_, Nil_, Nil_ );
+    auto list_ = app( Pair_, list_i, app( Pair_, Nil_, list_i ) );
+    auto program = abs( abs( app( Y_, app( program_inner->eval( ), var( 2 ) ), var( 1 ) ) ) );
+    auto current = app( program, Null_, list_ );
+    std::cout << static_cast< std::string >(*program) << std::endl;
     std::cout << static_cast< std::string >(*current) << std::endl;
     while ( true )
     {
